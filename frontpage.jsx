f3c2951f0611e1f2995420b7e3788418b27dd586
sparta8675309 <118635558+sparta8675309@users.noreply.github.com>
2023-10-26 15:11:19 -0700

    Create frontpage.jsx

---
+++ frontpage.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import './signup.css'
+function ClassScheduleInput() {
+
+    
+
+    return (
+        <div>
+        <h1> UCLA Carpool Connect</h1>
+            <div>
+                <button type="submit">Sign In</button>
+            </div>
+            <br></br>
+            <div>
+                <button type="submit">Sign In</button>
+            </div>
+        </div>
+    );
+}
+
+export default ClassScheduleInput;

bca988977095d467dbd5b10fb346c99cd9bd1fa1
sparta8675309 <118635558+sparta8675309@users.noreply.github.com>
2023-10-26 16:24:14 -0700

    Create schedule.jsx

---
+++ schedule.jsx
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from 'react';
+import './signup.css'
+function ClassScheduleInput() {
+
+    
+
+    return (
+        <div>
+            <h1>Schedule View Page</h1>
+            <table border="22" >
+                <tr>
+                    <td></td>
+                    <th> Monday </th>
+                    <th> Tuesday </th>
+                    <th> Wednesday </th>
+                    <th> Thursday </th>
+                    <th> Friday </th>
+                </tr>
+                <tr>
+                    <th>7:00-8:00</th>
+                    <td>Ride with Reuben to UCLA</td>
+                    <td></td>
+                    <td>Ride with Jacob to UCLA</td>
+                    <td></td>
+                    <td>Ride with Joshua to UCLA</td>
+                </tr>
+                <tr>
+                    <th>8:00-9:00</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>9:00-10:00</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>10:00-11:00</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>Ride with Jonah Back Home</td>
+                </tr>
+                <tr>
+                    <th>11:00-12:00</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>12:00-1:00</th>
+                    <td></td>
+                    <td></td>
+                    <td>Ride with Jacob Back Home</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>1:00-2:00</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>Ride with Jonah to UCLA</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>2:00-3:00</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>3:00-4:00</th>
+                    <td></td>
+                    <td>Ride with Ray to UCLA</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>4:00-5:00</th>
+                    <td></td>
+                    <td>Ride with Ray back Home</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>5:00-6:00</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>Ride with Joshua Back Home</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>6:00-7:00</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>7:00-8:00</th>
+                    <td>Ride with Reuben back Home</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+    );
+}
+
+export default ClassScheduleInput;

2a5622b13b60b3d95afde81c5d8e70ca6414d8f2
sparta8675309 <118635558+sparta8675309@users.noreply.github.com>
2023-10-26 15:09:26 -0700

    Create signin.jsx

---
+++ signin.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import './signup.css'
+function ClassScheduleInput() {
+    const [input1, setInput1] = useState('');
+    const [input2, setInput2] = useState('');
+    const [input3, setInput3] = useState('');
+    const [input4, setInput4] = useState('');
+    
+
+    return (
+        <div>
+            <h1>UCLA Carpool Connect</h1>
+            <div>
+                <h3>Username</h3>
+                <input
+                    type="text"
+                    name="input1"
+                    value={input1}
+                    onChange={(e) => setInput1(e.target.value)}
+                />
+            </div>
+            <div>
+                <h3>Password</h3>
+                <input
+                    type="text"
+                    name="input2"
+                    value={input2}
+                    onChange={(e) => setInput2(e.target.value)}
+                />
+            </div>
+           <br></br>
+            <div>
+                <button type="submit">Sign In</button>
+            </div>
+        </div>
+    );
+}
+
+export default ClassScheduleInput;

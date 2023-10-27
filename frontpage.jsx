336b263cadb028d102f4243b691274ec63f3da6a
sparta8675309 <118635558+sparta8675309@users.noreply.github.com>
2023-10-27 13:04:05 -0700

    Update frontpage.jsx

--- frontpage.jsx
+++ frontpage.jsx
@@ -8,7 +8,7 @@ function ClassScheduleInput() {
         <div>
         <h1> UCLA Carpool Connect</h1>
             <div>
-                <button type="submit">Sign In</button>
+                <button type="submit">Sign Up</button>
             </div>
             <br></br>
             <div>

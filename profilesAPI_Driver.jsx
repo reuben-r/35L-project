811201bd126e89f6d340bd7aa07e00f4a015fbbb
sparta8675309 <118635558+sparta8675309@users.noreply.github.com>
2023-11-28 20:47:49 -0800

    Update profilesAPI_Driver.jsx

--- profilesAPI_Driver.jsx
+++ profilesAPI_Driver.jsx
@@ -6,6 +6,9 @@ const DistanceCalculator = () => {
   const place2 = "90096";
   const place3 = "90071";
   const [map, setMap] = useState(null);
+  const [selectedDay, setSelectedDay] = useState("");
+  const [selectedOption, setSelectedOption] = useState("arrival");
+  const [requestStatus, setRequestStatus] = useState(null);
 
   useEffect(() => {
     const script = document.createElement("script");
@@ -13,11 +16,11 @@ const DistanceCalculator = () => {
     script.async = true;
     script.defer = true;
 
-    script.onload = () => {
+    document.head.appendChild(script);
+
+    window.initMap = () => {
       initMap();
     };
-
-    document.head.appendChild(script);
 
     return () => {
       document.head.removeChild(script);
@@ -32,11 +35,6 @@ const DistanceCalculator = () => {
         if (status === "OK" && results[0]) {
           const location = results[0].geometry.location;
           createMap(location);
-        } else {
-          console.error(
-            "Geocode was not successful for the following reason:",
-            status,
-          );
         }
       });
     }
@@ -45,7 +43,7 @@ const DistanceCalculator = () => {
   const createMap = (center) => {
     const newMap = new window.google.maps.Map(document.getElementById("map"), {
       center: center,
-      zoom: 12,
+      zoom: 10,
     });
 
     setMap(newMap);
@@ -55,10 +53,8 @@ const DistanceCalculator = () => {
     addMarker(newMap, place3, "School", "yellow");
   };
 
-  const [requestStatus, setRequestStatus] = useState(null); // State to track request status
-
   const addMarker = (map, location, label, color, link) => {
-    if (location) {
+    if (map) {
       const geocoder = new window.google.maps.Geocoder();
       geocoder.geocode({ address: location }, (results, status) => {
         if (status === "OK" && results[0]) {
@@ -72,26 +68,69 @@ const DistanceCalculator = () => {
           if (label !== "Home" && label !== "School") {
             marker.addListener("click", () => {
               const shouldRequestRide = window.confirm(
-                `Do you want to book a drive for ${label}?`,
+                `Do you want to request a ${
+                  selectedOption === "arrival" ? "ride to" : "ride from"
+                } ${label} on ${selectedDay}?`,
               );
               if (shouldRequestRide) {
-                setRequestStatus(`You have booked a Drive with ${label}`);
-                // Perform any additional actions when the request is submitted
+                setRequestStatus(
+                  `Request to ${label} on ${selectedDay} submitted`,
+                );
+                // Send to Backend
+              } else {
+                setSelectedDay(null);
+                setSelectedOption("arrival");
+                setRequestStatus(null);
               }
             });
           }
-        } else {
-          console.error(
-            "Geocode was not successful for the following reason:",
-            status,
-          );
         }
       });
     }
   };
 
+  const handleDaySelect = (day) => {
+    setSelectedDay(day);
+  };
+
+  const handleOptionSelect = (event) => {
+    setSelectedOption(event);
+  };
+
+  const handleAdditionalRequests = () => {
+    setSelectedDay(null);
+    setSelectedOption("arrival");
+    setRequestStatus(null);
+  };
+
   return (
     <div>
+      <div>
+        <label>Select Day:</label>
+        <select onChange={(e) => handleDaySelect(e.target.value)}>
+          <option value="Monday">Monday</option>
+          <option value="Tuesday">Tuesday</option>
+          <option value="Wednesday">Wednesday</option>
+          <option value="Thursday">Thursday</option>
+          <option value="Friday">Friday</option>
+          <option value="Saturday">Saturday</option>
+          <option value="Sunday">Sunday</option>
+        </select>
+      </div>
+      <div>
+        <label>Select Option:</label>
+        <select onChange={(e) => handleOptionSelect(e.target.value)}>
+          <option value="arrival">Arrival</option>
+          <option value="departure">Departure</option>
+        </select>
+      </div>
+      {requestStatus && (
+        <div>
+          <button onClick={handleAdditionalRequests}>
+            Make Additional Requests
+          </button>
+        </div>
+      )}
       <div id="map" style={{ height: "400px", width: "100%" }}></div>
       {requestStatus && <p>{requestStatus}</p>}
     </div>
